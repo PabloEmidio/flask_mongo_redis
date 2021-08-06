0ae9d8b33c49a6b4a55eef3f9c791a548920a43e
Pablo Santos <p.emidiodev@gmail.com>
2021-08-06 19:30:44 -0300

    Add models files

---
+++ banks/models/models.py
@@ -0,0 +1,82 @@
+from typing import List
+
+from dotenv import dotenv_values
+import pymongo
+import redis
+
+config = dotenv_values('.env')
+
+class MongoDatabaseModel:
+    LOCALURI = 'mongodb://mongo/admin'
+
+    def __init__(self, db_name: str='testbanks', collection: str='banks'):
+        mongo_uri = config.get('mongouri') or self.LOCALURI
+        client = pymongo.MongoClient(mongo_uri)
+        self.db = client[db_name]
+        self.db = self.db[collection]
+
+    def get_by_query(self, query: dict={}):
+        return [match for match in self.db.find(query)]
+
+
+    def insert(self, document: dict={}) -> None:
+        inserted = self.db.insert_one(document)
+        return len(str(inserted.inserted_id)) > 0
+
+
+    def insert_many(self, documents: List[dict]):
+        inserted = self.db.insert_many(documents)
+        return len(inserted.inserted_ids) > 0
+
+
+    def update(self, update_which: dict={}, update_set: dict={}):
+            updated = self.db.update_one(
+                update_which, update_set
+            )
+            return updated.modified_count == 1
+ 
+    def delete(self, delete_which: dict={}):
+        deleted = self.db.delete_one(delete_which)
+        return deleted.deleted_count == 1
+
+class RedisDatabaseModel:
+        LOCALURI = 'redis'
+
+        def __init__(self):
+            redis_uri = config.get('redisuri') or self.LOCALURI
+            self.db = redis.StrictRedis(host=redis_uri, port=6379, db=0, charset='UTF-8', decode_responses=True)
+
+        def get_keys(self, match: str='banks:*'):
+            keys = self.db.scan(0, match)[1]
+            keys.sort()
+            return keys
+
+        def get_values(self, bank_key: str = '', info_key: str=''):
+            try:
+                banks = []
+                if not bank_key or not info_key:
+                    for key in self.get_keys():
+                        banks.append(self.db.hgetall(key))
+                else:
+                    banks.append(self.db.hgetall(bank_key))
+                    banks.append(self.db.hgetall(info_key))
+                return banks
+            except Exception as error:
+                print(error)
+
+        def insert(self, bank_fields: dict[str, str], bank_infos: dict[str, str]):
+            cod_bank = bank_fields['cod']
+            try: 
+                assert self.db.hset(f'banks:{cod_bank}', mapping=bank_fields), 'There was an error'
+                assert self.db.hset(f'banks:{cod_bank}:infos', mapping=bank_infos), 'There was an error'
+            except Exception as error:
+                print(error)
+
+        def update(self, hash_name: str = '', bank_fields: dict[str, str]={'key': 'value'}):
+            updated = self.db.hset(hash_name, mapping=bank_fields)
+            print(updated)
+            return updated == 0
+        
+        def delete(self, bank_key: str = '', info_key: str=''):
+            deleted = self.db.delete(bank_key, info_key)
+            return deleted >= 1
---
+++ banks/models/sample_banks.py
@@ -0,0 +1,105 @@
+MONGO_SAMPLE_BANKS = [
+    {
+        'bank': 'Banco Itau S.A',
+        'pop_name': 'Itau',
+        'cod': 341,
+        'infos': {
+            'is_digital': False,
+            'website': 'itau.com.br',
+            'has_investment_platform': True
+        }
+    },
+    {
+        'bank': 'Caixa Econômica Federal',
+        'pop_name': 'Caixa',
+        'cod': 104,
+        'infos': {
+            'is_digital': False,
+            'website': 'caixa.gov.br',
+            'has_investment_platform': False
+        }
+    },
+    {
+        'bank': 'Banco Inter S.A',
+        'pop_name': 'Inter',
+        'cod': 77,
+        'infos': {
+            'is_digital': True,
+            'website': 'bancointer.com.br',
+            'has_investment_platform': True
+        }
+    },
+    {
+        'bank': 'Nu Pagamentos S.A',
+        'pop_name': 'Nubank',
+        'cod': 260,
+        'infos': {
+            'is_digital': True,
+            'website': 'nubank.com.br',
+            'has_investment_platform': False
+        }
+    },
+    {
+        'bank': 'Banco Neon S.A',
+        'pop_name': 'Neon',
+        'cod': 735,
+        'infos': {
+            'is_digital': True,
+            'website': 'banconeon.com.br',
+            'has_investment_platform': False
+        }
+    },
+]
+
+REDIS_SAMPLE_BANKS = [
+    {
+        'bank': 'Banco Itau S.A',
+        'pop_name': 'Itau',
+        'cod': 341,
+    },
+    {
+        'is_digital': '0',
+        'website': 'itau.com.br',
+        'has_investment_platform': '1',
+    },
+    {
+        'bank': 'Caixa Econômica Federal',
+        'pop_name': 'Caixa',
+        'cod': 104,
+    },
+    {
+        'is_digital': '0',
+        'website': 'caixa.gov.br',
+        'has_investment_platform': '0'
+    },
+    {
+        'bank': 'Banco Inter S.A',
+        'pop_name': 'Inter',
+        'cod': 77,
+    },
+    {
+        'is_digital': '1',
+        'website': 'bancointer.com.br',
+        'has_investment_platform': '1'
+    },
+    {
+        'bank': 'Nu Pagamentos S.A',
+        'pop_name': 'Nubank',
+        'cod': 260,
+    },
+    {
+        'is_digital': '1',
+        'website': 'nubank.com.br',
+        'has_investment_platform': '0'
+    },
+    {
+        'bank': 'Banco Neon S.A',
+        'pop_name': 'Neon',
+        'cod': 735,
+    },
+    {
+        'is_digital': '1',
+        'website': 'banconeon.com.br',
+        'has_investment_platform': '0'
+    }
+]
