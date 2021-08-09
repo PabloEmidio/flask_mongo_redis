9918b559201eeaaf37dc8db69dcfa378b8244534
Pablo Santos <p.emidiodev@gmail.com>
2021-08-09 09:29:55 -0300

    Migrate mongoviews to flask-restful

--- banks/views/mongoviews.py
+++ banks/views/mongoviews.py
@@ -1,25 +1,22 @@
-from flask import (
-    Flask, jsonify, redirect,
-    abort, request
-)
+from flask import Flask, redirect, abort, request
+from flask_restful import Resource, Api
 from bson.objectid import ObjectId
 
 from banks.models.models import MongoDatabaseModel
 from banks.models.sample_banks import MONGO_SAMPLE_BANKS
 
-def init_app(app: Flask)  -> None:
+def init_app(app: Flask) -> None:
+    api = Api(app)
     database = MongoDatabaseModel()
 
-    @app.route('/mongo/')
-    def mongo_add_sample_banks():
-        if not len(database.get_by_query()) > 0:
-            database.insert_many(MONGO_SAMPLE_BANKS)
-        return redirect('/mongo/banks/', 308)
-
-
-    @app.route('/mongo/banks/', methods=['GET', 'POST'])
-    def mongo_list_banks():
-        if request.method == 'GET':
+    class MongoAddSampleBanks(Resource):
+        def get(self):
+            if not len(database.get_by_query()) > 0:
+                database.insert_many(MONGO_SAMPLE_BANKS)
+            return redirect('/mongo/banks/', 308)
+    
+    class MongoListAndCreateBanks(Resource):
+        def get(self):
             banks, status_code = [], 200
             for document in database.get_by_query():
                 bank = {}
@@ -28,34 +25,34 @@ def init_app(app: Flask)  -> None:
                         bank[k] = v
                 banks.append(bank)
             if not banks:
-                return redirect('/mongo/', 307)
-        if request.method == 'POST':
+                return redirect('/mongo/', 303)
+            return banks, status_code
+
+        def post(self):
             try:
                 assert_message = 'Document must have {}'
-                banks, status_code = request.json, 201
-                assert isinstance(banks, dict), assert_message.format('only one')
-                assert 'bank' in banks.keys(), assert_message.format('bank')
-                assert 'pop_name' in banks.keys(), assert_message.format('pop_name')
-                assert 'cod' in banks.keys(), assert_message.format('cod')
-                assert 'infos' in banks.keys(), assert_message.format('infos')
-                assert 'is_digital' in banks['infos'].keys(), assert_message.format('infos.is_digital')
-                assert 'website' in banks['infos'].keys(), assert_message.format('infos.website')
-                database.insert(banks)
-                return redirect(f'/mongo/{banks["cod"]}/', 303)
+                bank, status_code = request.json, 323
+                assert isinstance(bank, dict), assert_message.format('only one')
+                assert 'bank' in bank.keys(), assert_message.format('bank')
+                assert 'pop_name' in bank.keys(), assert_message.format('pop_name')
+                assert 'cod' in bank.keys(), assert_message.format('cod')
+                assert 'infos' in bank.keys(), assert_message.format('infos')
+                assert 'is_digital' in bank['infos'].keys(), assert_message.format('infos.is_digital')
+                assert 'website' in bank['infos'].keys(), assert_message.format('infos.website')
+                database.insert(bank)
+                return redirect(f'/mongo/{bank["cod"]}/', status_code)
             except AssertionError as error:
                 status_code = 400
-                banks = {
+                bank = {
                     'message': str(error),
                     'status_code': status_code
                 }
             except Exception:
                 abort(500)
-        return jsonify(banks), status_code
+            return bank, status_code
 
-
-    @app.route('/mongo/<int:bank_cod>/', methods=['GET', 'PUT', 'DELETE'])
-    def mongo_bank_by_cod(bank_cod):
-        if request.method == 'GET':
+    class MongoBankByCod(Resource):
+        def get(self, bank_cod):
             try:
                 status_code = 200
                 bank = database.get_by_query(
@@ -66,7 +63,9 @@ def init_app(app: Flask)  -> None:
                 abort(404)
             except Exception:
                 abort(500)
-        if request.method == 'PUT':
+            return bank, status_code
+        
+        def put(self, bank_cod):
             try:
                 update_assert_message = 'An error happened updating {}'
                 bank, status_code, update_which = request.json, 201, {'cod': bank_cod}
@@ -97,7 +96,7 @@ def init_app(app: Flask)  -> None:
                         assert isinstance(hip, bool), f'{hip} must be boolean'
                         assert database.update(
                             update_which, {'$set': {'infos.has_investment_platform': hip}}
-                        ), update_assert_message.formaftf(f'{hip}')
+                        ), update_assert_message.format(f'{hip}')
                 return redirect(f'/mongo/{bank_cod}/')
             except AssertionError as error:
                 status_code = 400
@@ -108,8 +107,9 @@ def init_app(app: Flask)  -> None:
             except Exception as error:
                 print(error)
                 abort(500)
-
-        if request.method == 'DELETE':
+            return bank, status_code
+        
+        def delete(self, bank_cod):
             try:
                 assert database.delete({"cod": bank_cod}), ''
                 return redirect('/mongo/banks/')
@@ -117,5 +117,7 @@ def init_app(app: Flask)  -> None:
                 abort(404)
             except Exception:
                 abort(500)
-
-        return jsonify(bank), status_code
+    
+    api.add_resource(MongoAddSampleBanks, '/mongo/')
+    api.add_resource(MongoListAndCreateBanks, '/mongo/banks/')
+    api.add_resource(MongoBankByCod, '/mongo/<int:bank_cod>/')
